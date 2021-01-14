3f4dc5296bb0141ef4a7fdc0b373161739bf7001
Siddharth P <siddharth.padmakumar2002@gmail.com>
2021-01-15 01:08:02 +0530

    updates to reset board

--- chess_board.py
+++ chess_board.py
@@ -1,27 +1,26 @@
 #BOARD AND DISPLAY
 white_pieces = {'king':'♔','queen':'♕','bishop':'♗','rook':'♖','knight':'♘','pawn':'♙'}
 black_pieces = {'king':'♚','queen':'♛','bishop':'♝','rook':'♜','knight':'♞','pawn':'♟︎'}
-
+columns = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
 class ChessBoard:
     def __init__(self):
-        self.board = [] 
+        self.board = ['#'] 
         for n in range(8):
-            row = []
+            row = ['#']
             for i in range(8):
                 row.append(' ')
             self.board.append(row)
 b = ChessBoard()
-
 def display_board(board):
     
     
     print('   A   B   C   D   E   F   G   H')
     for i in range(8):
-        print(f' ---------------------------------\n{8-i}| {b.board[i][0]} | {b.board[i][1]} | {b.board[i][2]} | {b.board[i][3]} | {b.board[i][4]} | {b.board[i][5]} | {b.board[i][6]} | {b.board[i][7]} |{8-i}')
+        print(f' ---------------------------------\n{8-i}| {b.board[8-i][1]} | {b.board[8-i][2]} | {b.board[8-i][3]} | {b.board[8-i][4]} | {b.board[8-i][5]} | {b.board[8-i][6]} | {b.board[8-i][7]} | {b.board[8-i][8]} |{8-i}')
     print(' ---------------------------------')
     print('   A   B   C   D   E   F   G   H')
 
-#display_board(b.board) - to display board
+display_board(b.board) #- to display board
 
 class Pieces:
     def __init__(self):
@@ -54,3 +53,42 @@ class Pieces:
         self.black_pieces.append(self.black_bishops)
 pieces = Pieces()
 #can call pieces by pieces.black_pieces etc.
+
+def place_piece(board,piece,row,column):
+    board[row][columns[column]]=piece
+    
+'''
+place_piece(b.board,white_pieces,'pawn',2,'d')
+display_board(b.board)
+'''
+def reset_board(board):
+    for n in range(len(pieces.white_pawns)):
+        place_piece(b.board,pieces.white_pawns[n],2,list(columns.keys())[n])
+    for n in range(len(pieces.black_pawns)):
+        place_piece(b.board,pieces.black_pawns[n],7,list(columns.keys())[n])
+
+    place_piece(b.board,pieces.white_rooks[0],1,list(columns.keys())[0])
+    place_piece(b.board,pieces.white_rooks[1],1,list(columns.keys())[7])
+    place_piece(b.board,pieces.black_rooks[0],8,list(columns.keys())[0])
+    place_piece(b.board,pieces.black_rooks[1],8,list(columns.keys())[7])
+
+    place_piece(b.board,pieces.white_knights[0],1,list(columns.keys())[1])
+    place_piece(b.board,pieces.white_knights[1],1,list(columns.keys())[6])
+    place_piece(b.board,pieces.black_knights[0],8,list(columns.keys())[1])
+    place_piece(b.board,pieces.black_knights[1],8,list(columns.keys())[6])
+
+    place_piece(b.board,pieces.white_bishops[0],1,list(columns.keys())[2])
+    place_piece(b.board,pieces.white_bishops[1],1,list(columns.keys())[5])
+    place_piece(b.board,pieces.black_bishops[0],8,list(columns.keys())[2])
+    place_piece(b.board,pieces.black_bishops[1],8,list(columns.keys())[5])
+
+    place_piece(b.board,pieces.white_kings[0],1,list(columns.keys())[4])
+    place_piece(b.board,pieces.white_queens[0],1,list(columns.keys())[3])
+    place_piece(b.board,pieces.black_kings[0],8,list(columns.keys())[4])
+    place_piece(b.board,pieces.black_queens[0],8,list(columns.keys())[3])
+        
+
+reset_board(b.board)
+display_board(b.board)
+
+
